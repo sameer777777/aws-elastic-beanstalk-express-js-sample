161aa622f1f7a83a3d274115b37b4a03862111e8
sahamed <sahamed@netapp.com>
2025-05-04 16:21:32 +0530

    Update root route response to include styled HTML welcome message

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bye6s6 World!'));
+app.get('/', (req, res) => {
+    res.send(`
+        <div style="text-align: center; font-family: Arial, sans-serif; margin-top: 50px;">
+            <h1 style="color: #4CAF50;">Welcome to My App!</h1>
+            <p style="font-size: 18px;">This is a simple Express.js application running on AWS Elastic Beanstalk.</p>
+        </div>
+    `);
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
