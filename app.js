d859cad235cced71453d66976cda0c4d1cd209ac
sahamed <sahamed@netapp.com>
2025-05-05 15:39:51 +0530

    skill lab commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 8080;
 app.get('/', (req, res) => {
     res.send(`
         <div style="text-align: center; font-family: Arial, sans-serif; margin-top: 50px;">
-            <h1 style="color: #4CAF50;">Welcome to My App!</h1>
+            <h1 style="color: #4CAF50;">Welcome to My Skill lab!</h1>
             <p style="font-size: 18px;">This is a simple Express.js application running on AWS Elastic Beanstalk.</p>
         </div>
     `);
